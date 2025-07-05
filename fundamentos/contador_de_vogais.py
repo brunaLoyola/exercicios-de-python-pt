ee8b09134ea5e1fa84dc2b6fa2552d75e720983e
Bruna Loyola <brunloyola02@gmail.com>
2025-07-04 22:47:09 -0300

    feat: contar número de vogais em nome informado pelo usuário
    - O script recebe o nome via input, transforma em minúsculas
    - E conta quantas vogais aparecem, imprimindo o total.

---
+++ fundamentos/contador_de_vogais.py
@@ -0,0 +1,10 @@
+# Contar a quantidade de vogais que aparece em um nome informado pelo usuário
+
+vogais = 'aeiou'
+soma = 0
+nome = list(input('Digite seu nome: ').lower())
+
+for contador in nome:
+    if contador in vogais:
+        soma += 1
+print(f'Seu nome possui {soma} vogais')

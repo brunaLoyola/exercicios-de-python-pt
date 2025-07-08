dccf9d0eaddd58f92946693204be4e52adcfc95b
Bruna Loyola <brunloyola02@gmail.com>
2025-07-07 22:42:53 -0300

    fix: corrigida lógica para exibir todas as posições do maior e menor valor
    - Anteriormente, o código usava .index(), retornando apenas a primeira ocorrência
    - Agora, percorre a lista completa e armazena todas as posições corretas

--- listas/maior_e_menor_valor_lista.py
+++ listas/maior_e_menor_valor_lista.py
@@ -5,14 +5,30 @@ Lê cinco números e informa o maior e o menor com suas posições.
 """
 
 valores = []
+posicao_maior = []
+posicao_menor = []
 
 for c in range(0, 5):
     valores.append(int(input(f'Digite o {c+1}° valor: ')))
 
 maior = max(valores)
 menor = min(valores)
-posicao_maior = valores.index(maior)
-posicao_menor = valores.index(menor)
 
-print(f'O maior valor é o {maior} e está na {posicao_maior+1}° posição')
-print(f'O menor valor é o {menor} e está na {posicao_menor+1}° posição')
+for i in range(len(valores)):
+    if valores[i] == maior:
+        posicao_maior.append(i)
+
+for i in range(len(valores)):
+    if valores[i] == menor:
+        posicao_menor.append(i)
+
+print(posicao_maior)
+print(posicao_menor)
+
+print(f'O maior valor é o {maior} e está na ', end='')
+for v in posicao_maior:
+    print(f'{v+1}° posição...', end='')
+
+print(f'\nO menor valor é o {menor} e está na ', end='')
+for v in posicao_menor:
+    print(f'{v+1}° posição... ', end='')

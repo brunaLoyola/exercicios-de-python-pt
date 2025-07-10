40daea7edca38c5daf22de81354cb2679fc8a31b
Bruna Loyola <brunloyola02@gmail.com>
2025-07-09 21:37:35 -0300

    feat: calcular operações aritméticas básicas entre dois números
    - Recebe dois números e exibe soma, subtração, multiplicação,
    divisão inteira, potência e resto da divisão.

---
+++ fundamentos/operacoes_aritmeticas.py
@@ -0,0 +1,16 @@
+"""
+Exercício:
+
+Receba dois números do usuário e
+calcule os resultados das operações aritméticas básicas.
+"""
+
+numero01 = int(input('Digite o primeiro número: '))
+numero02 = int(input('Digite o segundo número: '))
+
+print(f'A soma é: {numero01 + numero02}.')
+print(f'A subtração é: {numero01 - numero02}.')
+print(f'A multiplicação é: {numero01 * numero02}.')
+print(f'A divisão comum é: {numero01 // numero02}.')
+print(f'A potência é: {numero01 ** numero02}.')
+print(f'O resto da divisão é: {numero01 % numero02}.')

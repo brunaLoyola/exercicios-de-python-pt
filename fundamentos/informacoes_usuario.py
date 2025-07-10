00986cd14d9bbdadb58c1ef7cca71a4fd6ac776f
Bruna Loyola <brunloyola02@gmail.com>
2025-07-09 21:26:30 -0300

    style: substituir aspas duplas por simples no código-fonte

--- fundamentos/informacoes_usuario.py
+++ fundamentos/informacoes_usuario.py
@@ -9,13 +9,13 @@ from datetime import datetime
 
 ano_atual = datetime.now().year
 
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-peso = float(input("Digite seu peso: "))
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu peso: '))
 
 ano_nascimento = ano_atual - idade
 
 print(
-    f"Seu nome é {nome}, você tem {idade} anos\n"
-    f"Nasceu no ano de {ano_nascimento}, seu peso é {peso}kg"
+    f'Seu nome é {nome}, você tem {idade} anos\n'
+    f'Nasceu no ano de {ano_nascimento}, seu peso é {peso}kg'
 )

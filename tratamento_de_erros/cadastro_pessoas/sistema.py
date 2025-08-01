d9dfce3f039a38eee3a67ac5c4552af7f1ba6b70
Bruna Loyola <brunloyola02@gmail.com>
2025-07-31 23:01:29 -0300

    feat: integra módulo de arquivo ao sistema de cadastro
    - Adicionado pacote lib.arquivo com as funções:
      - arquivo_existe(): verifica existência do arquivo.
      - criar_arquivo(): cria arquivo .txt caso não exista.
      - ler_arquivo(): lê conteúdo do arquivo com cabeçalho.
    
    - Modificado sistema.py para:
      - Verificar existência de 'bruna.txt' ao iniciar.
      - Utilizar ler_arquivo() ao selecionar a opção 1 no menu.

--- tratamento_de_erros/cadastro_pessoas/sistema.py
+++ tratamento_de_erros/cadastro_pessoas/sistema.py
@@ -1,24 +1,33 @@
 """
-Curso em video: Sistema de Cadastro
+Curso em Vídeo: Sistema de Cadastro
 
 Apresenta um menu interativo com três opções:
 1 - Ver pessoas cadastradas
 2 - Cadastrar nova pessoa
 3 - Sair do sistema
 
-O programa utiliza módulos separados para interface,
+O programa utiliza módulos separados para interface e manipulação de arquivos,
 com tratamento de exceções para entradas inválidas.
+
+Ao iniciar, o sistema verifica se o arquivo de cadastro existe.
+Se não existir, cria automaticamente um arquivo .txt para armazenar os dados.
 """
 
+from lib.arquivo import arquivo_existe, criar_arquivo, ler_arquivo
 from lib.interface import menu, cabecalho
 from time import sleep
+
+arq = 'bruna.txt'
+
+if not arquivo_existe(arq):
+    criar_arquivo(arq)
 
 while True:
     resposta = menu(
         ['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema']
     )
     if resposta == 1:
-        cabecalho('Opção 01')
+        ler_arquivo(arq)
     elif resposta == 2:
         cabecalho('Opção 02')
     elif resposta == 3:
